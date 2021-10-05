7159813c56770c33ff37578767cbb5a23f915dda
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2021-10-05 15:03:31 -0700

    Dropdown menu items added

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,26 +2,44 @@
 const bioSamples = d3.json('samples.json');
 
 bioSamples.then(function(a) {
-	// console.log(a.samples[1]['otu_ids']);
+	// console.log(a.samples[0]['otu_ids']);
     console.log(a);
     var namesArray = Object.values(a.names);
     var metaDataArray = Object.values(a.metadata);
     var samplesArray = Object.values(a.samples);
-    console.log(`namesArray is ${namesArray}`);
+    // console.log(`namesArray is ${namesArray}`);
    
-    console.log(`name is ${a.names[1]}`);
-    console.log(`Metadata ethnicity is ${a.metadata[1]['ethnicity']}`);
-    console.log(`otu_ids are ${a.samples[1]['otu_ids']}`)
-    console.log(`sample_values are ${a.samples[1]['sample_values']}`);
-    // console.log(`otu_labels are ${a.samples[1]['otu_labels']}`)
-    // const names = a.names;
-    // const metadata = a.metadata;
-    // const samples = a.samples;
-
+    // console.log(`name is ${a.names[0]}`);
+    // console.log(`Metadata ethnicity is ${a.metadata[0]['ethnicity']}`);
+    // console.log(`otu_ids are ${a.samples[0]['otu_ids']}`)
+    // console.log(`sample_values are ${a.samples[0]['sample_values']}`);
+    // console.log(`otu_labels are ${a.samples[0]['otu_labels']}`)
     // for (i = 0; i < names.length; i++) {
 
     // }
+
     initPlot();
+
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    // Use the names array to populate the dropdown menu
+    for (i = 0; i < namesArray.length; i++) {
+        d3.select("#selDataset").append("option").text(namesArray[i]);
+    }
+    // var opt1 = d3.select("#selDataset").append("option").text("904");
+
+    // // Create a new element (2 step method: create element, change it)
+    // var li1 = d3.select("ul").append("li");
+    // li1.text("A new item has been added!");
+
+    // // Use chaining to create a new element and set its text (single step)
+    // var li2 = d3.select("ul").append("li").text("Another new item!");
+
+    // Add an element and/or style it:  
+    // First, var li_new = d3.select(ID/class).append("li")
+    // Li_new.text("This is the new item").style("color","red"); 
+
+
 
     // For bar chart:
     // * Use `sample_values` as the values.
@@ -30,11 +48,13 @@ bioSamples.then(function(a) {
     function initPlot() {
         let title = `Test Title`;
         let labels = samplesArray[0]['otu_ids'];
-        let values = samplesArray[0]['sample_values']
+        let values = samplesArray[0]['sample_values'];
+        let hovertext = samplesArray[0]['otu_labels'];
 
         let trace1 = {
         x: values,
         y: labels,
+        text: hovertext,
         type: "bar",
         orientation: "h"
         };
