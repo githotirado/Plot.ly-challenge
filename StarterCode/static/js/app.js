d5cb753a69a7b9a138804becfc52c3a446d7ff48
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2021-10-04 21:18:08 -0700

    inspect samples structure

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,6 +2,14 @@
 const bioSamples = d3.json('samples.json');
 
 bioSamples.then(function(a) {
-	console.log(a);
-})
+	// console.log(a.samples[1]['otu_ids']);
+    console.log(a);
+    console.log(`name is ${a.names[1]}`);
+    console.log(`Metadata ethnicity is ${a.metadata[1]['ethnicity']}`);
+    console.log(`otu_ids are ${a.samples[1]['otu_ids']}`)
+    console.log(`sample_values are ${a.samples[1]['sample_values']}`);
+    console.log(`otu_labels are ${a.samples[1]['otu_labels']}`)
+});
 
+// let otuIDs = bioSamples.map(a => a.samples.otu_ids);
+// console.log(otuIDs);
