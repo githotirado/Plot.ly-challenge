1a5e013d1833114e3aacd1e4baad48bb7b0ff245
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2021-10-07 14:59:51 -0700

    added bonus gauge

--- StarterCode/plots.js
+++
@@ -1,8 +0,0 @@
-// Function to address the selection in the dropdown menu
-function optionChanged(myValue) {
-    var dropdownMenu = d3.select("#selDataset");
-    console.log(`You selected ${myValue}`);
-    plotCharts(myValue);
-    showDemographics(myValue);
-
-}
--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,17 @@
-//     // How to Add an element and/or style it:  
-//     // First, var li_new = d3.select(ID/class).append("li")
-//     // Li_new.text("This is the new item").style("color","red"); 
 //
 // Functions section
 //
 
-// Draw the bar, bubble charts based on nameID
+// Draw the bar, bubble, [bonus] gauge charts based on nameID
 function plotCharts(myID) {
     // Filter for the array with records containing myID
     let mySamples = samplesArray.filter(s => s.id == myID);
+    console.log(`Filtered samples array:`);
     console.log(mySamples);
+
+    // Get wash frequency from metaData
+    let metaRecord = metaDataArray.filter(s => s.id == myID);
+    let wfreq = metaRecord[0].wfreq;
 
     // For bar chart (use 'reverse()' to have taller bars on top):
         // * Use `sample_values` as the values.
@@ -65,10 +67,33 @@ function plotCharts(myID) {
 
     let layoutBubble = {
         title: bTitle,
+        height: 600,
         width: 800
     };
     
     Plotly.newPlot("bubble", dataBubble, layoutBubble);
+
+    // For the guage chart
+    var dataGauge = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: wfreq,
+            title: { text: "Speed" },
+            type: "indicator",
+            mode: "gauge+number"
+        }
+    ];
+    
+    var layoutGauge = { 
+        width: 600,
+        height: 500,
+        margin: { 
+            t: 0,
+            b: 0
+        }
+    };
+    // Plot the gauge chart
+    Plotly.newPlot('gauge', dataGauge, layoutGauge);
 }
 
 // Clear and populate the demographics panel
@@ -88,6 +113,15 @@ function showDemographics(myID) {
     }
 };
 
+// Function to address the selection in the dropdown menu. Redraw
+function optionChanged(myValue) {
+    // var dropdownMenu = d3.select("#selDataset");
+    console.log(`User selected ${myValue}`);
+    plotCharts(myValue);
+    showDemographics(myValue);
+
+}
+
 // main function to initialize dashboard at first nameID record
 function init() {
     // Use d3 library to read in samples.json
@@ -96,8 +130,8 @@ function init() {
     // Set up the promise, set up initial elements when complete
     bioSamples.then(function(myData) {
         console.log(myData);
-        // Capture each top level array. Leave as GLOBAL (no declaration)
-        //   because other functions access them!!
+        // Capture each top level array.
+        // LEAVE AS GLOBAL (no declaration) for other functions to access
         namesArray = Object.values(myData.names);
         metaDataArray = Object.values(myData.metadata);
         samplesArray = Object.values(myData.samples);
